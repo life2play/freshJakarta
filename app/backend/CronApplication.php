533d010d90c72382acb7e99f9082f7226c11077e
Arief M Utama <arief.utama@gmail.com>
2014-04-26 13:17:06 +0700

    Added tables and functions to get inter-halte eta
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- app/backend/CronApplication.php
+++ app/backend/CronApplication.php
@@ -101,7 +101,6 @@ class CronApplication extends Application {
   public function getAction()
   {
     $this->blockOutside();
-    $this->blockOutside();
     $url = $this->request->get('url');
     $checktime = $this->request->get('checktime');
     $table = $this->request->get('table');
@@ -132,34 +131,69 @@ class CronApplication extends Application {
         . "c.lat as haltelat, c.long as haltelong, a.latitude as buslat, "
         . "a.longitude as buslong, a.eta as buseta "
         . "from busway_eta_bus a join busway_halte b on a.koridorno = b.koridorno and a.haltename = b.haltename "
-        . "join busway_halte c on a.koridorno = c.koridorno and b.halteid = c.halteid limit 1" ;
-
+        . "join busway_halte c on a.koridorno = c.koridorno and b.halteid = c.halteid "
+        . "where checktime::text like '2014-04-%:15:%'" ;
 
     $url = "origin=%s&destination=%s";
 
     $db = Singleton::acquire("\\PetakUmpet\\Database");
 
     $res = $db->queryFetchAll($q);
+    $dt = new Model('busway_eta_interim_speed_distance'); 
+    $cc = new Model('cache_distance_query');
+
+    $ccquery = "select distance from cache_distance_query where srclat = ? and srclong = ? and dstlat = ? and dstlong = ?";
 
     if ($res) {
       foreach ($res as $row) {
-        $srclatlong = $row['buslat'].'%20'.$row['buslong'];
-        $dstlatlong = $row['haltelat'].'%20'.$row['haltelong'];
+        $srclat = round($row['buslat'], 4, PHP_ROUND_HALF_DOWN);
+        $srclong = round($row['buslong'], 4, PHP_ROUND_HALF_DOWN);
+        $dstlat = round($row['haltelat'], 4, PHP_ROUND_HALF_DOWN);
+        $dstlong = round($row['haltelong'], 4, PHP_ROUND_HALF_DOWN);
 
-        $req = "https://maps.googleapis.com/maps/api/directions/json?" . sprintf($url, $srclatlong, $dstlatlong). "&units=metric&sensor=false&key=AIzaSyCyL7UQogftN8YAh2JeC99y0ltxIn2cYSY";
-        $dirdata = $this->getJsonFromURL($req);
-        $routes = $dirdata->routes;
-        $distance = 0;
-        foreach($routes as $r) {
-          foreach($r->legs as $l) {
-            $distance += (int) $l->distance->value;
+        $ccdistance = $db->queryFetchOne($ccquery, array($srclat, $srclong, $dstlat, $dstlong));
+
+        if (!$ccdistance || $ccdistance == '' || $ccdistance === null || $ccdistance == 0) {
+          $srclatlong = $srclat .'%20'. $srclong;
+          $dstlatlong = $dstlat .'%20'. $dstlong;
+
+          $req = "https://maps.googleapis.com/maps/api/directions/json?" . sprintf($url, $srclatlong, $dstlatlong). "&units=metric&sensor=false&key=AIzaSyCyL7UQogftN8YAh2JeC99y0ltxIn2cYSY";
+          $dirdata = $this->getJsonFromURL($req);
+          $routes = $dirdata->routes;
+          $distance = 0;
+          foreach($routes as $r) {
+            foreach($r->legs as $l) {
+              $distance += (int) $l->distance->value;
+            }
           }
+          $ccd = array(
+            'srclat' => $srclat,
+            'srclong' => $srclong,
+            'dstlat' => $dstlat,
+            'dstlong' => $dstlong,
+            'distance' => $distance,
+          );
+          $cc->save($ccd);
+        } else {
+          var_dump($ccdistance);
+          $distance = (integer) $ccdistance;
+          echo "Using cached distance<br>";
         }
+
         $speed = 1;
         if (is_numeric($distance)) {
           $speed = $distance/$row['buseta'];
         }
-        echo $row['id'] . ' speed (meter/menit):' . $speed;
+
+        $d = array(
+          'busway_eta_bus_id' => $row['id'],
+          'koridorno' => $row['koridorno'],
+          'halteid' => $row['halteid'],
+          'busdistance' => $distance,
+          'eta' => $row['buseta'],
+          'speed' => $speed,
+        );
+        $dt->save($d);
       }
     }
 
@@ -168,4 +202,6 @@ class CronApplication extends Application {
 
 // 0816 111 0808 (Sylviana Murni) Sylviana Murni@yahoo.com
 // 08111 77 0808
+// query to process eta data to eta between haltes
+// select c.checktime, b.*, a.ahalte, a.bhalte, a.distance as haltedistance, a.distance/b.speed as eta_between_halte  from busway_halte_distance a,  busway_eta_interim_speed_distance b join busway_eta_bus c on b.busway_eta_bus_id = c.id where a.distance >= b.busdistance and b.busdistance <> 0 and b.speed <> 0 and a.koridorno=b.koridorno and a.ahalte = b.halteid 
 }
