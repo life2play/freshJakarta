957b5a9767ce52492bcf8e68af03921ceb3a31f5
Arief M Utama <arief.utama@gmail.com>
2014-04-27 10:07:11 +0700

    Routing in one trayek
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- app/backend/ApiApplication.php
+++ app/backend/ApiApplication.php
@@ -15,7 +15,7 @@ class ApiApplication extends Application {
     $jsonQ = $this->request->get('q');
     $params = json_decode($jsonQ);
 
-    $query = "select id, lat, long as lng, '<strong>Busway</strong> Koridor: ' || koridor ||': '|| haltename || '<br>(Distance: ' || distance || 'm)' as label, 'busway' as type, distance from (select 'B'||b.id AS id, koridor, haltename, lat, long, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from busway_halte b ) srca ";
+    $query = "select id, lat, long as lng, '<strong>Busway</strong> Koridor: ' || koridor ||': '|| haltename || '<br>(Distance: ' || distance || 'm)' as label, 'busway' as type, distance from (select 'B'||b.id AS id, koridor, haltename, lat, long, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from busway_halte b ) srca WHERE distance <= :radius ";
 
     $query  .= "UNION select id, lat, lng, label || '<br>(Distance: ' || distance || 'm)' , type, distance from (select 'A'||b.id AS id, lat, long as lng, a.nama as label, a.jenis as type, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from trayek_umum_rute b join trayek_umum a on b.trayek_umum_id = a.id  ) srcb ";
     $query .= " WHERE distance <= :radius  ORDER BY distance desc ";
@@ -96,7 +96,11 @@ class ApiApplication extends Application {
     return null;
   }
 
-  private function getRouteById($srcTrayekId, $srcId, $dstTrayekId, $dstId)
+  private function traverseRoute($srcTrayekId, $srcId, $dstTrayekId, $dstId)
+  {
+  }
+
+  private function getRoutePath($srcTrayekId, $srcId, $dstTrayekId, $dstId)
   {
     $db = Singleton::acquire('\\PetakUmpet\\Database');
     $q = "SELECT * FROM routing_paths WHERE point_a = ? AND point_b = ?";
@@ -104,23 +108,55 @@ class ApiApplication extends Application {
 
     $res = $db->queryFetchAll($q, $params);
     if ($res) {
-    } else {
-      return null;
-    }
+        foreach ($res as $row) {
+            $trayekA = $row['path_a'];
+            $trayekB = $row['path_b'];
+        }
+    } 
+    return null;
   }
 
-  private function getRouteInOneTrayek($srcTrayekId, $srcId, $dstTrayekId, $dstId)
+  private function getRouteInOneTrayek($srcTrayekId, $srcId, $dstId, $type)
   {
-    if (strstr($srcId, 'B')) {
+    $db = Singleton::acquire('\\PetakUmpet\\Database');
+    $arr = array();
+
+    if ($type == 'B') {
       // busway
+      $sid = str_replace('B','', $srcId);
+      $did = str_replace('B','', $dstId);
+
+      $bigId = $did; $smallId = $sid;
+      if ((int) $sid > (int) $did) {
+        $bigId = $sid; $smallId = $did;
+      }
+
+      $q = "select 'Koridor-'||b.koridor|| ': ' || b.shname || '-' || b.fhname AS label, a.lat, a.long as lng, a.haltename  from busway_halte a join busway_koridor b on a.koridor = b.koridor WHERE koridor = ? AND halteid::integer >= ? AND halteid::integer <= ?";
+      $params = array($srcTrayekId, $smallId, $bigId);
+
+      $res = $db->queryFetchAll($q, $params);
+      if ($res) {
+        return $res;
+      }
     } else {
-      // non-busway
+      $sid = str_replace('A','', $srcId);
+      $did = str_replace('A','', $dstId);
+
+      $bigId = $did; $smallId = $sid;
+      if ((int) $sid > (int) $did) {
+        $bigId = $sid; $smallId = $did;
+      }
+ 
+      $q = "select b.nama as label, a.lat, a.long as lng, 'Halte' as nama from trayek_umum_rute a join trayek_umum b on a.trayek_umum_id = b.id where b.id = ? and a.id >=? and a.id <=?";
+
+      $params = array($srcTrayekId, $smallId, $bigId);
+
+      $res = $db->queryFetchAll($q, $params);
+      if ($res) {
+        return $res;
+      }
     }
-  }
-
-  private function traverseRoute($id) 
-  {
-
+    return $arr;
   }
 
   public function getRouteAction()
@@ -130,9 +166,28 @@ class ApiApplication extends Application {
     $jsonQ = $this->request->get('q');
     $params = json_decode($jsonQ);
     
-    $srcTrayekId = $this->getTrayekId($params['srcid']);
-    $dstTrayekId = $this->getTrayekId($params['dstid']);
+    $srcId = $params['srcid'];
+    $dstId = $params['dstid'];
 
+    $srclat = $params['srclat'];
+    $srclong = $params['srclong'];
+    $dstlat = $params['dstlat'];
+    $dstlong = $params['dstlong'];
+
+    $srcTrayekId = $this->getTrayekId($srcId);
+    $dstTrayekId = $this->getTrayekId($dstId);
+
+
+    if ($srcTrayekId == $dstTrayekId) {
+        $type = 'A';
+        if (strstr('B', $srcId)) {
+            $type = 'B';
+        }
+        $arr['result'] = $this->getRouteInOneTrayek($srcTrayekId, $srcId, $dstId, $type);
+        echo json_encode($arr);
+        exit;
+    }
+    return $this->getRoutePath($srcTrayekId, $srcId, $dstTrayekId, $dstId);
   }
 
 }
