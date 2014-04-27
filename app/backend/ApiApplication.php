bdbd4f5421011ea9e361e919facc6656003cfaae
Arief M Utama <arief.utama@gmail.com>
2014-04-27 08:06:37 +0700

    nearbyPoint added other trayek
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- app/backend/ApiApplication.php
+++ app/backend/ApiApplication.php
@@ -15,7 +15,10 @@ class ApiApplication extends Application {
     $jsonQ = $this->request->get('q');
     $params = json_decode($jsonQ);
 
-    $query = "select lat, long as lng, '<strong>Busway</strong> Koridor: ' || koridor ||': '|| haltename as label, 'busway' as type from (select koridor, haltename, lat, long, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from busway_halte b ) src where distance <= :radius ";
+    $query = "select lat, long as lng, '<strong>Busway</strong> Koridor: ' || koridor ||': '|| haltename as label, 'busway' as type, distance from (select koridor, haltename, lat, long, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from busway_halte b ) srca ";
+
+    $query  .= "UNION select lat, lng, label, type, distance from (select lat, long as lng, a.nama as label, a.jenis as type, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from trayek_umum_rute b join trayek_umum a on b.trayek_umum_id = a.id  ) srcb ";
+    $query .= " WHERE distance <= :radius  ORDER BY distance desc ";
 
     $query = str_replace('::srclat::', $params->k, $query );
     $query = str_replace('::srclong::', $params->A, $query );
@@ -23,10 +26,14 @@ class ApiApplication extends Application {
     $query_params['radius'] = '1000';
 
     $res = $db->queryFetchAll($query, $query_params);
+
+    $arr = array();
+    $arr['result'] = array();
     if ($res) {
         $arr['result'] = $res;
-        echo json_encode($arr);
     }
+
+    echo json_encode($arr);
     exit;
   }
 
