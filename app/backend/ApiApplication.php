ad4b8e7b7c4fecc3505b1c4fb6897d7560a14066
Arief M Utama <arief.utama@gmail.com>
2014-04-27 08:29:56 +0700

    Add details on nearby points, add theatre data
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- app/backend/ApiApplication.php
+++ app/backend/ApiApplication.php
@@ -15,15 +15,15 @@ class ApiApplication extends Application {
     $jsonQ = $this->request->get('q');
     $params = json_decode($jsonQ);
 
-    $query = "select lat, long as lng, '<strong>Busway</strong> Koridor: ' || koridor ||': '|| haltename as label, 'busway' as type, distance from (select koridor, haltename, lat, long, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from busway_halte b ) srca ";
+    $query = "select lat, long as lng, '<strong>Busway</strong> Koridor: ' || koridor ||': '|| haltename || '<br>(Distance: ' || distance || 'm)' as label, 'busway' as type, distance from (select koridor, haltename, lat, long, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from busway_halte b ) srca ";
 
-    $query  .= "UNION select lat, lng, label, type, distance from (select lat, long as lng, a.nama as label, a.jenis as type, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from trayek_umum_rute b join trayek_umum a on b.trayek_umum_id = a.id  ) srcb ";
+    $query  .= "UNION select lat, lng, label || '<br>(Distance: ' || distance || 'm)' , type, distance from (select lat, long as lng, a.nama as label, a.jenis as type, case when ::srclat:: = b.lat and ::srclong:: = b.long then 0 else ( 6371 * acos( cos( radians(::srclat::) ) * cos( radians( b.lat ) ) * cos( radians( b.long ) - radians(::srclong::) ) + sin( radians(::srclat::) ) * sin( radians( b.lat ) ) ) )  end *1000 AS distance from trayek_umum_rute b join trayek_umum a on b.trayek_umum_id = a.id  ) srcb ";
     $query .= " WHERE distance <= :radius  ORDER BY distance desc ";
 
     $query = str_replace('::srclat::', $params->k, $query );
     $query = str_replace('::srclong::', $params->A, $query );
 
-    $query_params['radius'] = '1000';
+    $query_params['radius'] = 500;
 
     $res = $db->queryFetchAll($query, $query_params);
 
@@ -50,7 +50,7 @@ class ApiApplication extends Application {
 
     $routeQuery = "SELECT haltename, lat, long from busway_halte where koridor = :koridor";
 
-    $res = $db->queryFetchAll($query, array('radius' => 1000));
+    $res = $db->queryFetchAll($query, array('radius' => 500));
     $arr = array();
     $n = 0;
     if ($res) {
